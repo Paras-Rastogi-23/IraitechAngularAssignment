965a83c869c40e9b83dffbd48bca90aee5eda346
Paras <rastogiparas38@gmail.com>
2020-10-31 22:19:37 +0530

    Generate few Components

---
+++ src/app/material/material/material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+
+const material = []
+
+@NgModule({
+  declarations: [],
+  imports: [
+    material
+  ],
+  exports: [
+    material
+  ]
+})
+export class MaterialModule { }
